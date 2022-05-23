87eeb68f4cd4f3a02aa9a1716260965ae2a22545
Harry <36096251+harry-sunhao@users.noreply.github.com>
2022-05-23 13:20:08 +0800

    Update SDHealthy.js

--- SDHealthy.js
+++ SDHealthy.js
@@ -10,10 +10,10 @@ hostname = ehtc.12320.gov.cn
 Date.prototype.format = function (format) {
     var date = {
         "M+": this.getMonth() + 1,
-        "d+": this.getDate() - 1,
+        "d+": this.getDate(),
         "h+": Math.round(Math.random()*(22-16)+16),
-        "m+": this.getMinutes(),
-        "s+": this.getSeconds(),
+        "m+": Math.round(Math.random()*(59-5)+5),
+        "s+": Math.round(Math.random()*(59-5)+5),
         "q+": Math.floor((this.getMonth() + 3) / 3),
         "S": this.getMilliseconds()
     };
@@ -30,6 +30,14 @@ Date.prototype.format = function (format) {
 
 var text = '{"success":true,"msg":null,"data":[{"result":"阴性","name":"东营市人民医院","time":"2022-05-11 15:08:15"},{"result":"阴性","name":"东营市人民医院","time":"2022-05-04 18:24:05"},{"result":"阴性","name":"东营市人民医院","time":"2022-04-30 13:06:43"}],"code":0}';
 var obj = JSON.parse(text);
-obj.data[0].time = new Date().format("YYYY-MM-dd hh:mm:ss");
+var dateTime=new Date();
+for (var i=0;i<3;i++)
+{ 
+    dateTime = dateTime.setDate(dateTime.getDate() - 2);
+    dateTime = new Date(dateTime);
+    print(dateTime.format("YYYY-MM-dd hh:mm:ss"));
+    obj.data[i].time = dateTime.format("YYYY-MM-dd hh:mm:ss");
+
+}
 
 $done({body: JSON.stringify(obj)});
